be733784795e31be9bd743ebf9a032e3eaf42eae
asdf <asdf@asdf.ru>
2016-11-13 18:37:47 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by olymp on 07.11.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+//        DeadLock deadLock=new DeadLock();
+//        deadLock.makeDeadlock();
+
+        ObjectPoolTest objectPoolTest = new ObjectPoolTest();
+        objectPoolTest.testPool();
+    }
+}
